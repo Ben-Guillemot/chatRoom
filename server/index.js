41a0580d0c03e9396ff921f88dcf00807f609ae1
Ben-Guillemot <ben.guillemot1@gmail.com>
2022-02-07 19:16:10 +0100

    test server - react ok

--- server/index.js
+++ server/index.js
@@ -19,6 +19,10 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
     console.log(`User connected : ${socket.id}`);
 
+    socket.on("join_room", (data) => {
+        socket.join(data)
+        console.log(`user with id ${socket.id} is connected to the room ${data}`);
+    })
     socket.on("disconnect", () => {
         console.log(`User disconnected : ${socket.id}`);
     })
